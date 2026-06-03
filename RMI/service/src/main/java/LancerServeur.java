ef8c189e0f8d5559d5b55b26335e775fb07bd782
nbela <nbelaib88@gmail.com>
2026-06-03 17:36:02 +0200

    small fix

--- RMI/service/src/main/java/LancerServeur.java
+++ RMI/service/src/main/java/LancerServeur.java
@@ -1,6 +1,6 @@
+import java.io.InputStream;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class LancerServeur {
@@ -9,13 +9,12 @@ public class LancerServeur {
         try {
             // Lecture du config.properties
             Properties props = new Properties();
-            try (InputStream in = LancerServeur.class.getClassLoader()
-                                                     .getResourceAsStream("config.properties")) {
+            try (InputStream in = LancerServeur.class.getClassLoader().getResourceAsStream("config.properties")) {
                 if (in == null) throw new RuntimeException("config.properties introuvable");
                 props.load(in);
             }
 
-            int port       = Integer.parseInt(props.getProperty("rmi.port", "1099"));
+            int port = Integer.parseInt(props.getProperty("rmi.port", "1099"));
             String rmiName = props.getProperty("rmi.name", "serviceRestaurant");
 
             // Création de l'annuaire et enregistrement du service
