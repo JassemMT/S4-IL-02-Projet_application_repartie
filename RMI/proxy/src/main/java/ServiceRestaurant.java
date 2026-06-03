014f8e767d17463d80078c918446bd6fa0e5f13c
nbela <nbelaib88@gmail.com>
2026-06-03 17:18:27 +0200

    fix merge

--- RMI/proxy/src/main/java/ServiceRestaurant.java
+++ RMI/proxy/src/main/java/ServiceRestaurant.java
@@ -3,13 +3,10 @@ import java.rmi.RemoteException;
 
 public interface ServiceRestaurant extends Remote {
 
-    String getRestaurants() throws RemoteException;
+    public String getRestaurants()
+            throws RemoteException;
 
-    String getPlats(
-        int idRestaurant
-    ) throws RemoteException;
-
-    String reserverTable(
+    public String reserverTable(
         String nom,
         String prenom,
         int convives,
@@ -17,19 +14,5 @@ public interface ServiceRestaurant extends Remote {
         int idRestaurant,
         String date,
         String heure
-    ) throws RemoteException;
-
-    String creerCommande(
-        int numReservation
-    ) throws RemoteException;
-
-    String ajouterPlatCommande(
-        int numCommande,
-        int numPlat,
-        int quantite
-    ) throws RemoteException;
-
-    String getCommande(
-        int numCommande
     ) throws RemoteException;
 }
