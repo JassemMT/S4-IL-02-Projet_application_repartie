f7e03e94b5464da602477b2dd63087b0393be487
JassemMT <jassem.tamourgh9@etu.univ-lorraine.fr>
2026-06-15 20:52:53 +0200

    ip serv

---
+++ nancy-carte/src/config.ts
@@ -0,0 +1,22 @@
+export interface ConfigurationInterface {
+  tileLayerUrl: string;
+  stationInformationUrl: string;
+  stationStatusUrl: string;
+  proxyUrl: string;
+  incidentsUrl: string;
+
+}
+
+export const CONFIG: ConfigurationInterface = {
+  tileLayerUrl: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+
+  stationInformationUrl:
+    "https://api.cyclocity.fr/contracts/nancy/gbfs/v2/station_information.json",
+
+  stationStatusUrl:
+    "https://api.cyclocity.fr/contracts/nancy/gbfs/v2/station_status.json",
+
+  proxyUrl: "http://100.64.80.244:8080",
+
+  incidentsUrl: "http://100.64.80.244:8080/incidents"
+};
